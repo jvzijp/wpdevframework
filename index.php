93a4a6401adfd76865f0acfb6e8ea9d3d4a38d35
Jacob van Zijp <jacobvanzijp@gmail.com>
2011-06-24 11:12:42 +0200

    test

---
+++ index.php
@@ -0,0 +1,8 @@
+<?php get_header(); // The Header ?> 
+<!-- Sample Loop, delete before release -->
+<?php // include (TEMPLATEPATH . '/loop.php' ); ?>
+<!-- End Sample -->
+<?php get_sidebar(); // The Sidebar ?>
+
+<?php get_footer(); // The Footer ?>
+
